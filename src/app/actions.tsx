b26177a2a5cdf4e78fef40e26882bddae4b469a8
Navya Shree <navyashr4@gmail.com>
2024-07-13 20:04:37 +0530

    works

--- src/app/actions.tsx
+++ src/app/actions.tsx
@@ -1,12 +1,25 @@
-'use server'
+'use server';
 
-import {createAI, getMutableAIState, streamUI} from "ai/rsc"
-import {CoreMessage, ToolInvocation} from "ai"
-import { ReactNode } from "react";
-import {openai} from "@ai-sdk/openai"
-import { BotMessage } from "@/components/llm/message";
-import { Loader2 } from "lucide-react";
-import { z,symbol } from "zod";
+import { BotCard, BotMessage } from '@/components/llm-crypto/message';
+import { Price } from '@/components/llm-crypto/price';
+import { PriceSkeleton } from '@/components/llm-crypto/price-skeleton';
+import { Stats } from '@/components/llm-crypto/stats';
+import { StatsSkeleton } from '@/components/llm-crypto/stats-skeleton';
+import { env } from '@/env.mjs';
+import { openai } from '@ai-sdk/openai';
+import type { CoreMessage, ToolInvocation } from 'ai';
+import { createAI, getMutableAIState, streamUI } from 'ai/rsc';
+import { MainClient } from 'binance';
+import { Loader2 } from 'lucide-react';
+import type { ReactNode } from 'react';
+import { z } from 'zod';
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const binance = new MainClient({
+  api_key: env.BINANCE_API_KEY,
+  api_secret: env.BINANCE_API_SECRET,
+});
 
 /* 
   !-- The first implication of user interfaces being generative is that they are not deterministic in nature.
@@ -31,7 +44,7 @@ import { z,symbol } from "zod";
 
 
 const content = `\
-You are a crypto bot and you can help users get the prices of cryptocurrencies. 
+You are a crypto bot and you can help users get the prices of cryptocurrencies.
 
 Messages inside [] means that it's a UI element or a user event. For example:
 - "[Price of BTC = 69000]" means that the interface of the cryptocurrency price of BTC is shown to the user.
@@ -39,85 +52,199 @@ Messages inside [] means that it's a UI element or a user event. For example:
 If the user wants the price, call \`get_crypto_price\` to show the price.
 If the user wants the market cap or other stats of a given cryptocurrency, call \`get_crypto_stats\` to show the stats.
 If the user wants a stock price, it is an impossible task, so you should respond that you are a demo and cannot do that.
-If the user wants to do anything else unrelated to the function calls  \`get_crypto_stats\`  and  \`get_crypto_price\` , you can also chat with users and answer any questions they may have. 
+If the user wants to do anything else, it is an impossible task, so you should respond that you are a demo and cannot do that.
+
+Besides getting prices of cryptocurrencies, you can also chat with users.
 `;
 
-export const sendMessage = async(
-    message: string, 
-) : Promise<{
-    id: number;
-    role: "user" | "assistant";
-    display: ReactNode;
-}> => {
-    const history = getMutableAIState<typeof AI>()
+export async function sendMessage(message: string): Promise<{
+  id: number,
+  role: 'user' | 'assistant',
+  display: ReactNode;
+}> {
 
-    history.update([
-        ...history.get(),
-        {
-            role: "user",
-            content: "message"
-        }
-    ]);
+  const history = getMutableAIState<typeof AI>();
 
-    const reply = await streamUI({
-        model: openai("gpt-4o-2024-05-13"),
-        messages: [
+  history.update([
+    ...history.get(),
+    {
+      role: 'user',
+      content: message,
+    },
+  ]);
+
+  const reply = await streamUI({
+    model: openai('gpt-4o-2024-05-13'),
+    messages: [
+      {
+        role: 'system',
+        content,
+        toolInvocations: []
+      },
+      ...history.get(),
+    ] as CoreMessage[],
+    initial: (
+      <BotMessage className="items-center flex shrink-0 select-none justify-center">
+        <Loader2 className="h-5 w-5 animate-spin stroke-zinc-900" />
+      </BotMessage>
+    ),
+    text: ({ content, done }) => {
+      if (done) history.done([...history.get(), { role: 'assistant', content }]);
+
+      return <BotMessage>{content}</BotMessage>;
+    },
+    tools: {
+      get_crypto_price: {
+        description:
+          "Get the current price of a given cryptocurrency. Use this to show the price to the user.",
+        parameters: z.object({
+          symbol: z
+            .string()
+            .describe("The name or symbol of the cryptocurrency. e.g. BTC/ETH/SOL.")
+        }),
+        generate: async function* ({ symbol }: { symbol: string; }) {
+          yield (
+            <BotCard>
+              <PriceSkeleton />
+            </BotCard>
+          );
+
+          const stats = await binance.get24hrChangeStatististics({ symbol: `${symbol}USDT` });
+          // get the last price
+          const price = Number(stats.lastPrice);
+          // extract the delta
+          const delta = Number(stats.priceChange);
+
+          await sleep(1000);
+
+          history.done([
+            ...history.get(),
             {
-                role: "system", 
-                content, 
-                toolInvocations: []
+              role: 'assistant',
+              name: 'get_crypto_price',
+              content: `[Price of ${symbol} = ${price}]`,
             },
-            ...history.get()
-        ] as CoreMessage[],
-        initial: (
-            <BotMessage className="items-center flex shrink-0 select-none justify-center">
-                <Loader2 className="h-5 w-5 animate-spin stroke-zinc-900" />
-            </BotMessage>
-        ),
-        text: ({content, done}) => {
-            if (done) 
-                history.done([...history.get(), {role: "assistant", content}])
-            return <BotMessage>{content}</BotMessage>
+          ]);
+
+          return (
+            <BotCard>
+              <Price name={symbol} price={price} delta={delta} />
+            </BotCard>
+          );
         },
+      },
+      get_crypto_stats: {
+        description:
+          "Get the current stats of a given cryptocurrency. Use this to show the stats to the user.",
+        parameters: z.object({
+          slug: z
+            .string()
+            .describe("The full name of the cryptocurrency in lowercase. e.g. bitcoin/ethereum/solana.")
+        }),
+        generate: async function* ({ slug }: { slug: string; }) {
+          yield (
+            <BotCard>
+              <StatsSkeleton />
+            </BotCard>
+          );
 
-        tools: {
-            get_crypto_price: {
-                description: "Get the current price of a given cryptocurrency. Use this to show the price to the user.",
-                parameters: z.object({
-                    symbol: z.string().describe("")
-                })
-            }, 
-            get_crypto_stats: {}
-        }
+          const url = new URL("https://api.coinmarketcap.com/data-api/v3/cryptocurrency/detail");
 
-    });
+          // set the query params which are required
+          url.searchParams.append("slug", slug);
+          url.searchParams.append("limit", "1");
+          url.searchParams.append("sortBy", "market_cap");
 
+          const response = await fetch(url, {
+            headers: {
+              // set the headers
+              Accept: "application/json",
+              "Content-Type": "application/json",
+              "X-CMC_PRO_API_KEY": env.CMC_API_KEY,
+            }
+          });
 
-    return({
-        id: Date.now(), 
-        role: "assistant", 
-        display: <p>Hello!</p>
-    });
+          if (!response.ok) {
+            return <BotMessage>Crypto not found!</BotMessage>;
+          }
+
+          const res = await response.json() as {
+            data: {
+              id: number;
+              name: string;
+              symbol: string;
+              volume: number;
+              volumeChangePercentage24h: number;
+              statistics: {
+                rank: number;
+                totalSupply: number;
+                marketCap: number;
+                marketCapDominance: number;
+              },
+            };
+          };
+
+          const data = res.data;
+          const stats = res.data.statistics;
+
+          const marketStats = {
+            name: data.name,
+            volume: data.volume,
+            volumeChangePercentage24h: data.volumeChangePercentage24h,
+            rank: stats.rank,
+            marketCap: stats.marketCap,
+            totalSupply: stats.totalSupply,
+            dominance: stats.marketCapDominance,
+          };
+
+          await sleep(1000);
+
+          history.done([
+            ...history.get(),
+            {
+              role: 'assistant',
+              name: 'get_crypto_price',
+              content: `[Stats of ${data.symbol}]`,
+            },
+          ]);
+
+          return (
+            <BotCard>
+              <Stats {...marketStats} />
+            </BotCard>
+          );
+        },
+      }
+    },
+    temperature: 0,
+  });
+
+  return {
+    id: Date.now(),
+    role: 'assistant' as const,
+    display: reply.value,
+  };
 };
-
+// Define the AI state and UI state types
 export type AIState = Array<{
-    id?: number;
-    name?: "get_crypto_price" | "get_crypto_stats"; 
-    role: "user" | "assistant" | "system";
-    content: string; 
-}>
+  id?: number;
+  name?: 'get_crypto_price' | 'get_crypto_stats';
+  role: 'user' | 'assistant' | 'system';
+  content: string;
+}>;
 
 export type UIState = Array<{
-    id?: number; 
-    role: "user" | "assistant";
-    display: ReactNode; 
-    toolInvocations?: ToolInvocation[];
-}>
+  id: number;
+  role: 'user' | 'assistant';
+  display: ReactNode;
+  toolInvocations?: ToolInvocation[];
+}>;
 
+// Create the AI provider with the initial states and allowed actions
 export const AI = createAI({
-    initialAIState: [] as AIState, 
-    initialUIState: [] as UIState, 
-    actions: {
-        sendMessage
-    }
-})
+  initialAIState: [] as AIState,
+  initialUIState: [] as UIState,
+  actions: {
+    sendMessage,
+  },
+});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,74 +1,105 @@
-'use client';
+"use client";
 
-import ChatList from "@/components/chatList";
-import ChatScrollAnchor from "@/components/chatScrollAnchor";
-import { useEnterSubmit } from "@/lib/useEnterSubmit";
-import {SubmitHandler} from "react-hook-form"
-import TextareaAutosize from "react-textarea-autosize"
+import { ChatList } from "@/components/chat-list";
+import { ChatScrollAnchor } from "@/components/chat-scroll-anchor";
+import { UserMessage } from "@/components/llm-crypto/message";
 import { Button } from "@/components/ui/button";
-import {ArrowUpIcon} from "lucide-react"
-import { PlusIcon } from "lucide-react";
-import {z} from 'zod';
+import type { ChatInputs } from "@/lib/chat-schema";
+import { useEnterSubmit } from "@/lib/use-enter-submit";
+import { useForm } from "@/lib/use-form";
 import { useActions, useUIState } from "ai/rsc";
-import {AI} from "./actions"
-import { UserMessage } from "@/components/llm/message";
+import { ArrowDownIcon, PlusIcon } from "lucide-react";
 import { useEffect, useRef } from "react";
-import {useForm} from "react-hook-form"
+import type { SubmitHandler } from "react-hook-form";
+import TextareaAutosize from 'react-textarea-autosize';
+import type { AI } from "./actions";
 
-const chatSchema = z.object({
-  message: z.string().min(1, 'Message is required')
-})
+/*
+  !-- With language models becoming better at reasoning, we believe that there is a future where
+  !-- developers only write core application specific components while models take care of routing
+  !-- them based on the user's state in an application.
 
-export type ChatInput = z.infer<typeof chatSchema>;
+  !-- With generative user interfaces, the language model decides which user interface to render
+  !-- based on the user's state in the application, giving users the flexibility to interact with
+  !-- your application in a conversational manner instead of navigating through a series of predefined routes.
+*/
 
+// Here we can read the streamable UI using the sendMessage Server Action via a function call
+// render the returned UI like any other normal React components.
 export default function Home() {
-  const form = useForm<ChatInput>()
-  const {formRef, onKeyDown} = useEnterSubmit()
   const [messages, setMessages] = useUIState<typeof AI>();
   const { sendMessage } = useActions<typeof AI>();
-
+  const { formRef, onKeyDown } = useEnterSubmit();
   const inputRef = useRef<HTMLTextAreaElement>(null);
 
-  const onSubmit: SubmitHandler<ChatInput> = async(data) => {
+  const form = useForm<ChatInputs>();
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === '/') {
+        if (
+          e.target &&
+          ['INPUT', 'TEXTAREA'].includes((e.target as HTMLElement).nodeName)
+        ) {
+          return;
+        }
+        e.preventDefault();
+        e.stopPropagation();
+        if (inputRef?.current) {
+          inputRef.current.focus();
+        }
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [inputRef]);
+
+  const submitHandler: SubmitHandler<ChatInputs> = async (data) => {
     const value = data.message.trim();
     formRef.current?.reset();
-
     if (!value) return;
 
+    // Add user message UI
     setMessages(currentMessages => [
-      ...currentMessages, 
+      ...currentMessages,
       {
-        id: Date.now(), 
-        role: "user", 
-        display: <UserMessage>{value}</UserMessage>
+        id: Date.now(),
+        role: "user",
+        display: <UserMessage>{value}</UserMessage>,
       },
     ]);
 
     try {
+      // Submit and get response message
       const responseMessage = await sendMessage(value);
       setMessages(currentMessages => [
-        ...currentMessages, 
+        ...currentMessages,
         responseMessage,
       ]);
-    } catch(error) {
-      console.error(error)
+    } catch (error) {
+      // You may want to show a toast or trigger an error state.
+      console.error(error);
     }
-  
-  };
 
+  };
 
   return (
     <main>
       <div className="pb-[200px] pt-4 md:pt-10">
-        <ChatList messages={[]} />
-        <ChatScrollAnchor />
-        </div>
+
+        <ChatList messages={messages} />
+        <ChatScrollAnchor trackVisibility={true} />
+      </div>
       <div className="fixed inset-x-0 bottom-0 w-full bg-gradient-to-b from-muted/30 from-0% to-muted/30 to-50% duration-300 ease-in-out animate-in dark:from-background/10 dark:from-10% dark:to-background/80 peer-[[data-state=open]]:group-[]:lg:pl-[250px] peer-[[data-state=open]]:group-[]:xl:pl-[300px]">
         <div className="mx-auto sm:max-w-2xl sm:px-4">
           <div className="px-4 flex justify-center flex-col py-2 space-y-4 border-t shadow-lg bg-background sm:rounded-t-xl sm:border md:py-4 bg-white">
             <form
               ref={formRef}
-              onSubmit={form.handleSubmit(onSubmit)}
+              onSubmit={form.handleSubmit(submitHandler)}
             >
               <div className="relative flex flex-col w-full overflow-hidden max-h-60 grow bg-background sm:rounded-md sm:border">
                 <TextareaAutosize
@@ -89,7 +120,7 @@ export default function Home() {
                     size="icon"
                     disabled={form.watch('message') === ''}
                   >
-                    <ArrowUpIcon className="w-5 h-5" />
+                    <ArrowDownIcon className="w-5 h-5" />
                     <span className="sr-only">Send message</span>
                   </Button>
                 </div>
@@ -113,4 +144,3 @@ export default function Home() {
     </main>
   );
 }
-      
